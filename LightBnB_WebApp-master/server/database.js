b71fd9e171a37936a128f292d8d5a82b4e80238b
Sean Oyler <xrysen@gmail.com>
2020-11-26 18:57:46 +0000

    Adds ability to look up users by email, id and add users to database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -17,18 +17,24 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
+
+const getUserWithEmail = (email) => {
+  email = email.toLowerCase();
+  return pool.query (`
+  SELECT * 
+  FROM users
+  WHERE email = $1;
+  `, [email])
+  .then((res) => {
+    if (res.rows[0]) {
+      return res.rows[0];
     } else {
-      user = null;
+      return null;
     }
-  }
-  return Promise.resolve(user);
+  })
+  .catch(() => console.log(null));
 }
+
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -36,9 +42,23 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+
+const getUserWithId = (id) => {
+  const userId = id;
+  return pool.query(`
+  SELECT * 
+  FROM users
+  WHERE id = $1;
+  `, [userId])
+  .then((res) => {
+    if (res.rows[0]) {
+      return res.rows[0];
+    } else {
+      return null;
+    }
+  });
 }
+
 exports.getUserWithId = getUserWithId;
 
 
@@ -47,12 +67,23 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+// const addUser =  function(user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// }
+const addUser = (user) => {
+  const name = user.name;
+  const email = user.email;
+  const password = user.password;
+  return pool.query(`
+  INSERT INTO users (name, email, password) 
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `, [name, email, password]);
 }
+
 exports.addUser = addUser;
 
 /// Reservations
