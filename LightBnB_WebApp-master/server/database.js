b775c549c546b922e3b77611cc7503dc4372893d
Sean Oyler <xrysen@gmail.com>
2020-11-26 23:02:17 +0000

    Cleans up code a bit

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,6 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -8,7 +9,6 @@ const pool = new Pool({
   host: 'localhost',
   database: 'lightbnb'
 });
-
 
 /// Users
 
